3cc96322174e4f279c3c5ac57d319b0c956c9809
Kwon770 <sckwon770@gmail.com>
2020-04-16 08:14:11 +0900

    4-4 First Hooks Query

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo-hooks";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import Router from "./Router";
 
-export default () => (
-  <ThemeProvider theme={Theme}>
-    <>
-      <GlobalStyles />
-      <Router isLoggedIn={false} />
-    </>
-  </ThemeProvider>
-);
+const QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
+export default () => {
+  const {
+    data: { isLoggedIn },
+  } = useQuery(QUERY);
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <>
+        <GlobalStyles />
+        <Router isLoggedIn={false} />
+      </>
+    </ThemeProvider>
+  );
+};
