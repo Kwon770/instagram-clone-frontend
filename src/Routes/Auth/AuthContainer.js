0ddf8adb8f914900e3b381634b8b3877da6b20d1
Kwon770 <sckwon770@gmail.com>
2020-04-21 08:54:19 +0900

    5-4 requestSecret Mutation and Refactor

---
+++ src/Routes/Auth/AuthContainer.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import AuthPresenter from "./AuthPresenter";
+import useInput from "../../Hooks/useInput";
+import { useMutation } from "react-apollo-hooks";
+import { LOG_IN } from "./AuthQueris";
+
+export default () => {
+  const [action, setAction] = useState("logIn");
+  const username = useInput("");
+  const firstName = useInput("");
+  const lastName = useInput("");
+  const email = useInput("");
+
+  const requestSecret = useMutation(LOG_IN, {
+    variables: { email: email.value },
+  });
+
+  const onLogin = (e) => {
+    e.preventDefault();
+    if (email !== "") requestSecret();
+  };
+
+  return (
+    <AuthPresenter
+      setAction={setAction}
+      action={action}
+      username={username}
+      firstName={firstName}
+      lastName={lastName}
+      email={email}
+      onLogin={onLogin}
+    />
+  );
+};
