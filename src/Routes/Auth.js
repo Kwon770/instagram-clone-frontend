a7d2c31abe195dfa65629e4d3bf49ab1babef5bd
Kwon770 <sckwon770@gmail.com>
2020-04-21 08:12:18 +0900

    5-2 Footer Auth UI part Three

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,22 +1,44 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import Input from "../Components/Input";
+import Button from "../Components/Button";
 
 const Wrapper = styled.div`
   min-height: 80vh;
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
 `;
 
 const Box = styled.div`
   ${(props) => props.theme.whiteBox}
   border-radius: 0px;
-  width: 250px;
+  width: 100%;
+  max-width: 350px;
 `;
 
 const StateChanger = styled(Box)`
   text-align: center;
   padding: 20px 0px;
+`;
+
+const Form = styled(Box)`
+  padding: 40px;
+  padding-bottom: 30px;
+  margin-bottom: 15px;
+  form {
+    width: 100%;
+    input {
+      width: 100%;
+      &:not(:last-child) {
+        margin-bottom: 7px;
+      }
+    }
+    button {
+      margin-top: 10px;
+    }
+  }
 `;
 
 const Link = styled.span`
@@ -29,19 +51,24 @@ export default () => {
 
   return (
     <Wrapper>
-      <StateChanger>
+      <Form>
         {action === "logIn" ? (
-          <>
-            Don't have an account?{" "}
-            <Link onClick={() => setAction("signUp")}>Sing up</Link>
-          </>
+          <form>
+            <Input placeholder={"Username"} />
+            <Input placeholder={"Password"} />
+            <Button text={"Log in"} />
+          </form>
         ) : (
-          <>
-            Have an account?{" "}
-            <Link onClick={() => setAction("logIn")}>Log in</Link>
-          </>
+          <form>
+            <Input placeholder={"First name"} />
+            <Input placeholder={"Last name"} />
+            <Input placeholder={"Email"} />
+            <Input placeholder={"Username"} />
+            <Input placeholder={"Password"} />
+            <Button text={"Sign up"} />
+          </form>
         )}
-      </StateChanger>
+      </Form>
     </Wrapper>
   );
 };
