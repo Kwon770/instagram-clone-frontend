2fef41e2384e7d4f2cf9209db70761cda56ae85f
Kwon770 <sckwon770@gmail.com>
2020-04-29 08:26:37 +0900

    7-0 Getting the Feed and Loader Component

--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,1 +1,47 @@
-export default () => "Feed";
+import React from "react";
+import styled from "styled-components";
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo-hooks";
+import Loader from "../Components/Loader";
+
+const FEED_QUERY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        userName
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          userName
+        }
+      }
+      createdAt
+    }
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 70vh;
+`;
+
+export default () => {
+  const { data, loading } = useQuery(FEED_QUERY);
+  return <Wrapper>{loading && <Loader />}</Wrapper>;
+};
