8f20c2044f0c3269457b61346ce9191949f4c58f
Kwon770 <sckwon770@gmail.com>
2020-05-17 15:16:16 +0900

    9-1 Profile Screen part Two (previous commit is part one)

--- src/Routes/Profile/ProfilePresenter.js
+++ src/Routes/Profile/ProfilePresenter.js
@@ -1,15 +1,63 @@
 import React from "react";
 import styled from "styled-components";
+import { Helmet } from "react-helmet";
 import Loader from "../../Components/Loader";
 import Avatar from "../../Components/Avatar";
+import FatText from "../../Components/FatText";
+import FollowButton from "../../Components/FollowButton";
+import SquarePost from "../../Components/SquarePost";
 
 const Wrapper = styled.div`
-  min-height: 60vh;
+  min-height: 100vh;
 `;
 
-const Header = styled.header``;
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 80%;
+  margin: 0px auto;
+  margin-bottom: 40px;
+`;
 
 const HeaderColumn = styled.div``;
+
+const UserNameRow = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const UserName = styled.span`
+  font-size: 26px;
+  display: block;
+`;
+
+const Counts = styled.ul`
+  display: flex;
+  margin: 15px 0px;
+`;
+
+const Count = styled.li`
+  font-size: 14px;
+  &:not(:last-child) {
+    margin-right: 10px;
+  }
+`;
+
+const FullName = styled(FatText)`
+  font-size: 16px;
+`;
+
+const Bio = styled.p`
+  margin: 10px 0px;
+`;
+
+const Posts = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 200px);
+  grid-template-rows: 200px;
+  grid-auto-rows: 200px;
+`;
 
 export default ({ loading, data }) => {
   if (loading) {
@@ -21,6 +69,7 @@ export default ({ loading, data }) => {
   } else if (!loading && data && data.seeUser) {
     const {
       seeUser: {
+        id,
         avatar,
         userName,
         fullName,
@@ -34,13 +83,47 @@ export default ({ loading, data }) => {
       },
     } = data;
     return (
-      <>
+      <Wrapper>
+        <Helmet>
+          <title>{userName} | Prismagram</title>
+        </Helmet>
         <Header>
           <HeaderColumn>
             <Avatar size="lg" url={avatar} />
           </HeaderColumn>
+          <HeaderColumn>
+            <UserNameRow>
+              <UserName>{userName}</UserName>
+              {!isSelf && <FollowButton id={id} isFollowing={isFollowing} />}
+            </UserNameRow>
+            <Counts>
+              <Count>
+                <FatText text={postsCount.toString()} /> posts
+              </Count>
+              <Count>
+                <FatText text={followersCount.toString()} /> followers
+              </Count>
+              <Count>
+                <FatText text={followingCount.toString()} /> following
+              </Count>
+            </Counts>
+            <FatText text={fullName} />
+            <Bio>{bio}</Bio>
+          </HeaderColumn>
         </Header>
-      </>
+        <Posts>
+          {posts &&
+            posts.map((post) => (
+              <SquarePost
+                key={post.id}
+                id={post.id}
+                likeCount={post.likeCount}
+                commentCount={post.commentCount}
+                file={post.files[0]}
+              />
+            ))}
+        </Posts>
+      </Wrapper>
     );
   }
 };
